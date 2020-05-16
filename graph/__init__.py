93a087d8846cdcb422c9d0055d1d63da302abbea
Black-Tomcat <tomcat.frantz@gmail.com>
2020-05-16 19:12:55 +1000

    refactor imports to appropriate position

--- graph/__init__.py
+++ graph/__init__.py
@@ -1,4 +1,4 @@
 # don't remove these. They're here to register the models according to Mongoengine
 from graph.types.models.template import TemplateModel, TemplateSectionModel
-from graph.types.models.document import DocumentSectionModel, DocumentModel
+from graph.types.models.document import DocumentModel, DocumentSectionModel
 from graph.types.models.user import UserModel
--- graph/schema.py
+++ graph/schema.py
@@ -6,11 +6,12 @@ from graph.mutation import Mutation
 from graph.types.document import DocumentSection, Document
 from graph.types.template import TemplateSection, Template
 from graph.types.user import User
+
 from graph.types.utils.error import Error
 
 
 schema = Schema(
     query=Query,
     mutation=Mutation,
-    types=[User, DocumentSection, Document, Error, TemplateSection, Template],
+    types=[User, DocumentSection, Document, TemplateSection, Template, Error],
 )
